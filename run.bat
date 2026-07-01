663c4a1e4a5d015187f0b2ab82f892bf72cc9b3b
Kurth-S <ku.schuffenegger@duocuc.cl>
2026-07-01 00:47:26 -0400

    cambios

---
+++ run.bat
@@ -0,0 +1,6 @@
+set PORT=8082
+set DB_URL=jdbc:postgresql://aws-1-sa-east-1.pooler.supabase.com:6543/postgres?prepareThreshold=0
+set DB_USERNAME=postgres.lfrfksyqsfefohfuzcbr
+set DB_PASSWORD=FQFqnoR17PONJji7
+set MS_CATALOGO_URL=http://localhost:8081
+java -jar target\ms-pedidos-0.0.1-SNAPSHOT.jar
